0631037a1ece2eaeb4c9d0e234d578c6bec2bb3c
Andrew Cornfeld <cpm6gh@virginia.edu>
2022-11-23 22:09:20 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.speedrunpracticeprogram {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.speedrunpracticeprogram to javafx.fxml;
+    exports com.example.speedrunpracticeprogram;
+}
