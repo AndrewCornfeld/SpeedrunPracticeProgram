8c5cd9c8993bd79639ad11f826396186b43afb0f
Andrew Cornfeld <cpm6gh@virginia.edu>
2022-11-24 16:48:28 -0500

    added more to controller

--- src/main/java/com/example/speedrunpracticeprogram/HelloApplication.java
+++ src/main/java/com/example/speedrunpracticeprogram/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("on trick scene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("opening scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Speedrun Practice Program");
         stage.setScene(scene);
--- src/main/java/com/example/speedrunpracticeprogram/OpeningSceneController.java
+++ src/main/java/com/example/speedrunpracticeprogram/OpeningSceneController.java
@@ -1,14 +1,37 @@
 package com.example.speedrunpracticeprogram;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class OpeningSceneController {
+public class OpeningSceneController{
     @FXML
-    private Label welcomeText;
+    private Label newTrickNameLabel;
+    @FXML
+    private TextField newTrickNameTextField;
+    @FXML
+    private Button newTrickNameAddButton;
+    @FXML
+    public void initialize(){
+        newTrickNameLabel.setVisible(false);
+        newTrickNameTextField.setVisible(false);
+        newTrickNameAddButton.setVisible(false);
+    }
 
     @FXML
     protected void onNewTrickButtonClick() {
-        //lol
+        newTrickNameLabel.setVisible(true);
+        newTrickNameTextField.setVisible(true);
+        newTrickNameAddButton.setVisible(true);
     }
+    @FXML
+    protected void onNewTrickNameAddButtonClick() {
+
+    }
+
 }
