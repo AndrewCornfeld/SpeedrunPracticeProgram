ea681b6b5f95679441836d28f615d9ab52e82717
Andrew Cornfeld <cpm6gh@virginia.edu>
2022-11-23 22:10:04 -0500

    initial commit

--- src/main/java/com/example/speedrunpracticeprogram/HelloApplication.java
+++ src/main/java/com/example/speedrunpracticeprogram/HelloApplication.java
@@ -1,6 +1,6 @@
 package com.example.speedrunpracticeprogram;
 
-import javafx.application.Application;
+import  javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("on trick scene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Speedrun Practice Program");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/speedrunpracticeprogram/OpeningSceneController.java
+++ src/main/java/com/example/speedrunpracticeprogram/OpeningSceneController.java
@@ -3,12 +3,12 @@ package com.example.speedrunpracticeprogram;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class OpeningSceneController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onNewTrickButtonClick() {
+        //lol
     }
 }
