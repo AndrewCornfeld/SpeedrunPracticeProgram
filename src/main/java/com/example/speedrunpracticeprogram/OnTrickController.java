47a9ed2c1b0fb7b9ac7ad86895079aa110452909
Andrew Cornfeld <cpm6gh@virginia.edu>
2022-12-16 14:34:41 -0500

    starting OnTrickController

--- src/main/java/com/example/speedrunpracticeprogram/OnTrickController.java
+++ src/main/java/com/example/speedrunpracticeprogram/OnTrickController.java
@@ -1,4 +1,17 @@
 package com.example.speedrunpracticeprogram;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class OnTrickController {
+    @FXML
+    private Label
+    public OnTrickController(String trickName) {
+
+    }
+
+    @FXML
+    public void initialize(){
+
+    }
 }
