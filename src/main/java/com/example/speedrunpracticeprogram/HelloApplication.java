11976d80c59a40b7824c9750ce304fae58c39edc
Andrew Cornfeld <cpm6gh@virginia.edu>
2023-01-09 23:02:40 -0500

    refactoring

--- src/main/java/com/example/speedrunpracticeprogram/HelloApplication.java
+++ src/main/java/com/example/speedrunpracticeprogram/HelloApplication.java
@@ -1,6 +1,6 @@
 package com.example.speedrunpracticeprogram;
 
-import  javafx.application.Application;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     private static Stage primaryStage;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("opening scene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("src/main/resources/opening scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Speedrun Practice Program");
         stage.setScene(scene);
@@ -27,8 +27,5 @@ public class HelloApplication extends Application {
     public static void changeScene(String fxml) throws IOException {
         Parent pane = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource(fxml)));
         primaryStage.getScene().setRoot(pane);
-//        if(fxml.equals("on trick scene.fxml")){
-//            OpeningSceneController openingSceneController = new OpeningSceneController();
-//        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module com.example.speedrunpracticeprogram {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
 
-    opens com.example.speedrunpracticeprogram to javafx.fxml;
+    opens com.example.speedrunpracticeprogram.main to javafx.fxml;
     exports com.example.speedrunpracticeprogram;
 }
